aabb12156fb109e166ea8675e8f29b0930237119
Blake Morrison <blakemorrison@Blakes-MacBook-Pro.local>
2020-05-09 15:12:07 -0400

    ready to deploy

---
+++ pages/admin/index.js
@@ -0,0 +1,56 @@
+import Layout from '../../components/Layout';
+import Admin from '../../components/auth/Admin';
+import Link from 'next/link';
+
+const AdminIndex = () => {
+    return (
+        <Layout>
+            <Admin>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5">
+                            <h2>Admin Dashboard</h2>
+                        </div>
+                        <div className="col-md-4">
+                            <ul class="list-group">
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/category-tag">
+                                        <a>Create Category</a>
+                                    </Link>
+                                </li>
+
+                                <li className="list-group-item">
+                                
+                                        <a href="/admin/crud/category-tag">Create Tag</a>
+                                    
+                                </li>
+
+
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/blog">
+                                        <a>Create Blog</a>
+                                    </Link>
+                                </li>
+
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/blogs">
+                                        <a>Update/Delete Blogs</a>
+                                    </Link>
+                                </li>
+
+                                <li className="list-group-item">
+                                    <Link href="/user/update">
+                                        <a>Update Profile</a>
+                                    </Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Admin>
+        </Layout>
+    );
+};
+
+export default AdminIndex;
